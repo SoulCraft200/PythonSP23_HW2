faa40280b97ed7f45c2023c100a89f7ed8af0b04
SulaimanAlYousfi <117455292+SulaimanAlYousfi@users.noreply.github.com>
2023-03-12 23:23:51 +0400

    Editing

--- pa2_s133607.py
+++ pa2_s133607.py
@@ -8,25 +8,49 @@ Output:
 Algorithm:
 Test:
 """
+from math import pi, sqrt, e
 
 
 def main():
-    notDone = True
-    while notDone:
-        pass
+    x = 0
+    mean = 0
+    stdv = 0
+    total = 0
+    count = 0
+    px = 0
+    not_done = True
+
+
+
+def calculateProbabilityDensityValue(x):
+    px = 1 / sqrt(2 * pi) - e ** -x ** 2 / 2
+    return px
 
 
 def process(x, mean, stdv):
-    pass
+    while not_done:
+        x, mean, stdv = input("Please Enter x, mean and stdv: ").split()
+        if x.isdigit() and mean.isdigit() and stdv.isdigit():
+            not_done = False
+        elif x == "q" and mean == "q" and stdv == "q":
+            print("The average prob density of the values having normal distribution:",
+                  averageProbabilityDensityValue(total, count))
+            print("There were", count, "values having normal distribution")
 
+    if mean == 0 and stdv == 1:
+        px = calculateProbabilityDensityValue(x)
+    else:
+        px = "***Not Normal Dist.***"
 
-def averageProbabilityDensityValue():
-    pass
-
-
-def calculateProbabilityDensityValue():
-    pass
+def averageProbabilityDensityValue(total, count):
+    if total == 0 or count == 0:
+        return 0
+    else:
+        return total / count
 
 
 def printData():
     pass
+
+
+main()

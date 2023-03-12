719dbfa4757495c58e1f91afce8fe962e6093b72
Soul <sulaimanalyousefi2003@gmail.com>
2023-03-12 21:21:59 +0400

    creating main

---
+++ pa2_s133607.py
@@ -0,0 +1,16 @@
+"""
+Name:Sulaiman Khalifa Khalfan Al Yousfi
+ID:s133607
+HW:2
+Purpose:
+Input:
+Output:
+Algorithm:
+Test:
+"""
+def main():
+    notDone = True
+    while notDone:
+        pass
+
+

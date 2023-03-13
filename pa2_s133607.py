df2e6bf263cb7f2b204c930d097a5d59071bc515
Soul <sulaimanalyousefi2003@gmail.com>
2023-03-13 12:19:27 +0400

    Edited

--- pa2_s133607.py
+++ pa2_s133607.py
@@ -11,15 +11,26 @@ Test:
 from math import pi, sqrt, e
 
 
+
+
 def main():
-    x = 0
-    mean = 0
-    stdv = 0
     total = 0
     count = 0
-    px = 0
-    not_done = True
+    temp = input("Please Enter x, mean and stdv: ").split()
+    while not temp[0] == "q" and not temp[1] == "q" and not temp[2] == "q":
+        if temp[0].isdigit() and temp[1].isdigit() and temp[2].isdigit():
+            temp[0] = float(temp[0])
+            temp[1] = float(temp[1])
+            temp[2] = float(temp[2])
+            process(temp[0], temp[1], temp[2], total, count)
+            temp = input("Please Enter x, mean and stdv: ").split()
+        else:
+            print("Invalid input....")
+            temp = input("Please Enter x, mean and stdv: ").split()
 
+    print("The average prob density of values having normal distribution:",
+          averageProbabilityDensityValue(total, count))
+    print("Ther were",count,"values having normal distribution.")
 
 
 def calculateProbabilityDensityValue(x):
@@ -27,20 +38,17 @@ def calculateProbabilityDensityValue(x):
     return px
 
 
-def process(x, mean, stdv):
-    while not_done:
-        x, mean, stdv = input("Please Enter x, mean and stdv: ").split()
-        if x.isdigit() and mean.isdigit() and stdv.isdigit():
-            not_done = False
-        elif x == "q" and mean == "q" and stdv == "q":
-            print("The average prob density of the values having normal distribution:",
-                  averageProbabilityDensityValue(total, count))
-            print("There were", count, "values having normal distribution")
-
+def process(x, mean, stdv, total, count):
+    px = 0
     if mean == 0 and stdv == 1:
         px = calculateProbabilityDensityValue(x)
+        total += px
+        count += 1
     else:
         px = "***Not Normal Dist.***"
+
+    printData(x, mean, stdv, px)
+
 
 def averageProbabilityDensityValue(total, count):
     if total == 0 or count == 0:
@@ -49,8 +57,15 @@ def averageProbabilityDensityValue(total, count):
         return total / count
 
 
-def printData():
-    pass
+def printData(x, mean, stdv, px):
+    print("%10s%10s%20s%50s" % ("x", "Mean", "standDev", "P(X) / MSG"))
+    print("%10s%10s%20s%50s" % ("---", "---------", "---------", "---------"))
+    px = str(px)
+    if px.isdigit():
+        px = float(px)
+        print("%10.2f%10.2f%20.2f%50.4f" % (x, mean, stdv, px))
+    else:
+        print("%10.2f%10.2f%20.2f%50s" % (x, mean, stdv, px))
 
 
 main()
